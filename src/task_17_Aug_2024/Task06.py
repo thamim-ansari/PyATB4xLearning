698548a61e95339d730737923715a2f572b11b95
thamim-ansari <thamimansari0117@gmail.com>
2024-08-19 17:28:18 +0530

    Task 6 of 17 Aug 2024 added

---
+++ src/task_17_Aug_2024/Task06.py
@@ -0,0 +1,8 @@
+# Task 6
+
+# - Develop a Python script that calculates the square and cube of a given number.
+
+num = int(input("Enter a number: "))
+
+print(f"Square of {num} is {num ** 2}")
+print(f"Cube of {num} is {num ** 3}")
