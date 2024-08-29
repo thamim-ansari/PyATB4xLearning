cb760f939237101238d5d566cadf3e7fdd461ea7
thamim-ansari <thamimansari0117@gmail.com>
2024-08-29 15:44:01 +0530

    Learning exercise for Aug 29 2024 added

---
+++ src/Learning_exercises/ex_29082024/Lab103.py
@@ -0,0 +1,11 @@
+set1 = set(["TheTestingAcademy", "For", "TheTestingAcademy."])
+print(len(set1))
+
+for i in set1:
+    print(i)
+
+set1.add("Thamim")
+set1.add("Thamim")
+
+name = "Thamim"
+print(name.upper())
