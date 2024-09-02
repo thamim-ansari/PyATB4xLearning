c067bc9904d3e1c7f747e6cd627e546fe98a728e
thamim-ansari <thamimansari0117@gmail.com>
2024-09-02 14:26:05 +0530

    Learning exercise for Aug 31 2024 added

---
+++ src/Learning_exercises/ex_31082024/Lab107.py
@@ -0,0 +1,41 @@
+class Person:
+    # Attributes
+    id = None
+    name = None
+    age = None
+    email = None
+    height = None
+    gender = None
+    phone_no = None
+    address = None
+
+    # Behaviour
+    def talk(self):  # NRNG  # self - this , self will be first argument in every behaviour.
+        print("I can talk")
+
+    def sleep(self, name):  # Arg with No Return
+        print("I am a Method!!")
+        print("Sleep", name)
+
+    def sleep2(self, name):  # Arg with Return
+        print("I am a Method!!")
+        return None
+
+    def walk(self):
+        print("I am walking")
+
+    def walk_return(self):  # No Arg with Return
+        return "I am walking"
+
+
+# Create an Object of the Class
+# ObjectRef = ClassName() -> Object
+Varun = Person()
+Varun.name = "Varun"
+print(Varun.name)
+Varun.talk()
+
+Arun = Person()
+Arun.name = "Arun"
+print(Arun.name)
+Arun.talk()
