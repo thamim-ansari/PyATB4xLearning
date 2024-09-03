4833f95c270a75d6ff9868a2eeabc3884b538094
thamim-ansari <thamimansari0117@gmail.com>
2024-09-03 16:37:31 +0530

    Learning exercise for Sept 03 2024 added

---
+++ src/Learning_exercises/Sept/ex_02092024/python_package/Lab114.py
@@ -0,0 +1,9 @@
+from src.Learning_exercises.Sept.ex_02092024.normal_dir.Lab113 import print_details_113
+
+
+def print_details_114():
+    print("I am print_details_in_Lab114")
+    print_details_113()
+
+
+print_details_114()
